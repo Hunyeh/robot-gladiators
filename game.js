00b4dd323997d4cb9d7bdb27a10e38954bc10cb1
Hunter <88465412+Hunyeh@users.noreply.github.com>
2021-08-30 19:03:49 -0400

    played with console logs

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-window.alert("this is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robots name");
+console.log(playerName);
+console.log(10+10)
+console.log("Our robot's name is "  + playerName);
+
+
+
+
+
+
+// fight();
